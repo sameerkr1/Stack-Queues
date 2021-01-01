f6345fe96c6d0e09aafbd8793dccd35ea5279510
Sameer kumar singh <60307463+sameerkr1@users.noreply.github.com>
2021-01-01 19:40:24 +0530

    Update NextGreaterElement.cpp

--- NextGreaterElement.cpp
+++ NextGreaterElement.cpp
@@ -1,8 +1,19 @@
-void nextGreaterElement(ll a[],ll n){
+vector<ll> nextGreaterElement(ll a[],ll n){
     stack<ll>st;
     st.push(0);                  /// think of decreasing stack and store index of every element
+    vector<ll>ans(n);
     
     for(ll i=1;i<n;i++){
-       if(st.top())
+        while(!st.empty()&&a[st.top()]<a[i]){
+            ll idx=st.top();
+            ans[idx]=a[i];
+            st.pop();
+        }
+        st.push(i);
     }
+    while(!st.empty()){
+       ans[st.top()]=-1;
+       st.pop();
+    }
+    return ans; 
 }
