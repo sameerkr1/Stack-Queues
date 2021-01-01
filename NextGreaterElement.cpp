e90119c6c11d7a2b74d548073631687ae1305bea
Sameer kumar singh <60307463+sameerkr1@users.noreply.github.com>
2021-01-01 19:33:16 +0530

    Create NextGreaterElement.cpp

---
+++ NextGreaterElement.cpp
@@ -0,0 +1,8 @@
+void nextGreaterElement(ll a[],ll n){
+    stack<ll>st;
+    st.push(0);                  /// think of decreasing stack and store index of every element
+    
+    for(ll i=1;i<n;i++){
+       if(st.top())
+    }
+}
